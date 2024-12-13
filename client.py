74b263e207b99469b951a398cfc9a535b928dd1f
Prakriti Timilsina <98209027+Dynamic-prakriti@users.noreply.github.com>
2024-12-13 21:00:57 +0545

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
-target_host = "127.0.0.1"
-target_port = 9999
+target_host = "target_ip"
+target_port = [port]
 def main():
     client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     client.connect((target_host,target_port))
